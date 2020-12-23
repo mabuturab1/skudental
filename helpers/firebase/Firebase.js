c51164f9d279975631df20b743a5c4d87eec56ae
mabuturab1 <m.abuturab1@gmail.com>
2020-12-23 12:39:50 +0500

    changed to react native firebase

--- helpers/firebase/Firebase.js
+++ helpers/firebase/Firebase.js
@@ -1,21 +1,24 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
 
-var firebaseConfig = {
-  apiKey: 'AIzaSyCuxI7dLcSit5rwFZnzNYdYNjVSWSURNPU',
-  authDomain: 'upwork-abuturab1.firebaseapp.com',
-  databaseURL: 'https://upwork-abuturab1.firebaseio.com',
-  projectId: 'upwork-abuturab1',
-  storageBucket: 'upwork-abuturab1.appspot.com',
-  messagingSenderId: '851288471005',
-  appId: '1:851288471005:web:39e59fd8b107a106dd3234',
-  measurementId: 'G-9YVWEWQGFF',
-};
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const database = firebase.firestore();
-const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+// var firebaseConfig = {
+//   apiKey: 'AIzaSyCuxI7dLcSit5rwFZnzNYdYNjVSWSURNPU',
+//   authDomain: 'upwork-abuturab1.firebaseapp.com',
+//   databaseURL: 'https://upwork-abuturab1.firebaseio.com',
+//   projectId: 'upwork-abuturab1',
+//   storageBucket: 'upwork-abuturab1.appspot.com',
+//   messagingSenderId: '851288471005',
+//   appId: '1:851288471005:web:39e59fd8b107a106dd3234',
+//   measurementId: 'G-9YVWEWQGFF',
+// };
+// firebase.initializeApp(firebaseConfig);
+// const auth = firebase.auth();
+// const database = firebase.firestore();
+// const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+// const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { auth, database, googleAuthProvider, facebookAuthProvider, firebase };
+// export { auth, database, googleAuthProvider, facebookAuthProvider, firebase };
+import auth from '@react-native-firebase/auth';
+const authModule = auth();
+export { authModule as auth };
